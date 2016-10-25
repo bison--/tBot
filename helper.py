3517c210cee26943334e52ee76c4d1954f5f6a47
bison-- <mike.bison42@googlemail.com>
2016-10-25 21:33:21 +0200

    realitycheck.pl

--- helper.py
+++ helper.py
@@ -8,28 +8,10 @@ def realitycheck(bot):
     :param bot: tBot
     """
     checks = [
-        '''13.06.2016 10:44 :~: Einmal Popcorn bitte. 50 Liter.
-
-        Reality
-        Ich höre: "Forever alone"
-
-        Story
-        Ich habe eine Szene aus Indiana Jones und der Tempel des Todes im Kopf und denke: "Die Insekten lassen dich niemals allein."''',
-
-        '''02.06.2016 20:52 :~: Saudi Arabi Money Rich
-
-        Reality
-        Ich lese auf Twitter: "Haftbefehl gegen mutmaßlichen IS-Terroristen."
-
-        Story
-        Ich frage mich, was mehr Terror bedeutet. So eine kleine Explosion oder 3:28 Gesinge von Chabos und Babos?''',
-
-        '''13.02.2016 17:54 :~: Die Stange war zu kurz
-
-        Reality
-        Eine neue Person betritt den Raum. Bewaffnet mit Hut und Heißklebepistole läuft sie zielstrebig auf jemanden zu. Dieser ist allerdings nicht zu Gesprächen aufgelegt...
-        Story
-        ... und sagt: "Gib mal kurz!", schnappt sich die Heißklebepistole, hält sie sich an die Schläfe und drückt ab.'''
+        '''13.06.2016 10:44 :~: Einmal Popcorn bitte. 50 Liter. http://realitycheck.pl/search/?q=Einmal+Popcorn+bitte.+50+Liter."''',
+        '''02.06.2016 20:52 :~: Saudi Arabi Money Rich http://realitycheck.pl/search/?q=Saudi+Arabi+Money+Rich''',
+        '''13.02.2016 17:54 :~: Die Stange war zu kurz http://realitycheck.pl/search/?q=Die+Stange+war+zu+kurz''',
+        '10.08.2016 15:09 :~: Artikelnummer 3 oder 0 http://realitycheck.pl/search/?q=Artikelnummer+3+oder+0'
     ]
     import random
     for line in random.choice(checks).split("\n"):
